bd42cdbd945e99833fdff0354ee54dbd6f8d0626
Daniel Rodrigues de Carvalho Filho <danircfilho@hotmail.com>
2022-01-16 20:19:09 -0300

    conect

--- db/conn.js
+++ db/conn.js
@@ -7,11 +7,12 @@ hostname: us-cdbr-east-05.cleardb.net
 mysql://b53bbe6209fe6e:e7335e84@us-cdbr-east-05.cleardb.net/heroku_4bc76cb70dc86b9?reconnect=true*/
 
 /* 'heroku_65908da8a2ffd8f', 'b2168d5717ddfa', 'd2049979' */
+/* mysql://b53bbe6209fe6e:e7335e84@us-cdbr-east-05.cleardb.net/heroku_4bc76cb70dc86b9?reconnect=true */
 
-const sequelize = new Sequelize('mysql://b53bbe6209fe6e:e7335e84@us-cdbr-east-05.cleardb.net/heroku_4bc76cb70dc86b9?reconnect=true' , {
+const sequelize = new Sequelize('heroku_4bc76cb70dc86b9', 'b53bbe6209fe6e', 'e7335e84',  {
   host: 'us-cdbr-east-05.cleardb.net',
-  port: 3000,
-  dialect: 'mysql',
+  port: 3306,
+  dialect: 'mysql'
 })
 
 try {
