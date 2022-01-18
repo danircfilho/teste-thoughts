354ce8ec2d71f2d1a60a923e65e06e63ae748473
Daniel Rodrigues de Carvalho Filho <danircfilho@hotmail.com>
2022-01-17 22:20:24 -0300

    conect-teste

--- db/conn.js
+++ db/conn.js
@@ -1,20 +1,6 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
-/* 
-database: heroku_4bc76cb70dc86b9
-user: b53bbe6209fe6e
-password: e7335e84
-hostname: us-cdbr-east-05.cleardb.net
-mysql://b53bbe6209fe6e:e7335e84@us-cdbr-east-05.cleardb.net/heroku_4bc76cb70dc86b9?reconnect=true
-*/
-/* const dbName = process.env.DB_NAME
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASSWORD
-const dbHost = process.env.DB_HOST
-const dbPort = process.env.DB_PORT */
-
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
